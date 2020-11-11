b0cd2db014d66fcf4dfb573ff64766e5d097326b
Dan Finlay <dan@danfinlay.com>
2020-11-11 09:45:09 -0800

    Remove timeout approach in favor of Proxy approach

--- src/web3Wrapper.js
+++ src/web3Wrapper.js
@@ -11,135 +11,113 @@ setupWeb3()
  */
 function setupWeb3 () {
 
-  if (window.ethereum) {
-    _setupWeb3()
-  } else {
-    detectEthereumProvider({ silent: true })
-      .then((_provider) => {
-        if (window.ethereum) {
-          _setupWeb3()
-        } else {
-          console.log(getExitMessage('Failed to detect window.ethereum.'))
-        }
-      })
-      .catch((error) => {
-        console.error(
-          getExitMessage('Unexpected error when detecting window.ethereum.'),
-          error,
-        )
-      })
+  // if used before MetaMask stops injecting window.web3
+  if (window.ethereum && window.ethereum.isMetaMask && window.web3) {
+    console.log(getExitMessage('Detected MetaMask-injected window.web3.'))
+    return
   }
 
-  function _setupWeb3 () {
+  if (window.web3) {
+    console.log(getExitMessage('Detected existing window.web3.'))
+    return
+  }
 
-    // if used before MetaMask stops injecting window.web3
-    if (window.ethereum && window.ethereum.isMetaMask && window.web3) {
-      console.log(getExitMessage('Detected MetaMask-injected window.web3.'))
+  if (window.ethereum && !window.ethereum.isMetaMask) {
+    console.warn(getMessage(
+      'Detected non-MetaMask window.ethereum. ' +
+      'Proceeding to initialize window.web3, but may experience undefined behavior.',
+    ))
+  }
+
+  if (!('autoRefreshOnNetworkChange' in window.ethereum)) {
+    window.ethereum.autoRefreshOnNetworkChange = true
+  }
+
+  /*
+   * We now construct a lazy provider in case this script is run before the ethereum provider is injected.
+   * This can happen either because this script is run in a different extension for backwards-compat,
+   * or because the provider is being injected late for another reason (platform specific).
+   */
+  const lazyProvider = new Proxy({}, {
+    get (_target, prop, receiver) {
+      return Reflect.get(window.ethereum, prop, receiver)
+    },
+  })
+
+  const web3 = new Web3(lazyProvider)
+
+  web3.setProvider = function () {
+    console.log(getMessage('Overrode web3.setProvider.'))
+  }
+  console.log(getMessage('Injected web3.'))
+
+  window.ethereum.on('accountsChanged', (accounts) => {
+    web3.eth.defaultAccount = Array.isArray(accounts) && accounts.length > 0
+      ? accounts[0]
+      : null
+  })
+
+  // export web3 as a global, checking for usage
+  let reloadInProgress = false
+  let lastTimeUsed
+  let previousChainId
+
+  const web3Proxy = new Proxy(web3, {
+    get: (_web3, key) => {
+      // get the time of use
+      lastTimeUsed = Date.now()
+      // return value normally
+      return _web3[key]
+    },
+    set: (_web3, key, value) => {
+      // set value normally
+      _web3[key] = value
+    },
+  })
+
+  Object.defineProperty(window, 'web3', {
+    enumerable: false,
+    writable: true,
+    configurable: true,
+    value: web3Proxy,
+  })
+
+  window.ethereum.on('chainChanged', (currentChainId) => {
+    // if the auto refresh on network change is false do not
+    // do anything
+    if (!window.ethereum.autoRefreshOnNetworkChange) {
       return
     }
 
-    if (window.web3) {
-      console.log(getExitMessage('Detected existing window.web3.'))
+    // if reload in progress, no need to check reload logic
+    if (reloadInProgress) {
       return
     }
 
-    if (window.ethereum && !window.ethereum.isMetaMask) {
-      console.warn(getMessage(
-        'Detected non-MetaMask window.ethereum. ' +
-        'Proceeding to initialize window.web3, but may experience undefined behavior.',
-      ))
+    // set the initial chain
+    if (!previousChainId) {
+      previousChainId = currentChainId
+      return
     }
 
-    if (!('autoRefreshOnNetworkChange' in window.ethereum)) {
-      window.ethereum.autoRefreshOnNetworkChange = true
+    // skip reload logic if web3 not used
+    if (!lastTimeUsed) {
+      return
     }
 
-    /*
-     * We now construct a lazy provider in case this script is run before the ethereum provider is injected.
-     * This can happen either because this script is run in a different extension for backwards-compat,
-     * or because the provider is being injected late for another reason (platform specific).
-     */
-    const lazyProvider = new Proxy(window.ethereum, {
-      get (_target, prop, receiver) {
-        return Reflect.get(window.ethereum, prop, receiver)
-      },
-    })
-
-    const web3 = new Web3(lazyProvider)
-
-    web3.setProvider = function () {
-      console.log(getMessage('Overrode web3.setProvider.'))
+    // if chain did not change, exit
+    if (currentChainId === previousChainId) {
+      return
     }
-    console.log(getMessage('Injected web3.'))
 
-    window.ethereum.on('accountsChanged', (accounts) => {
-      web3.eth.defaultAccount = Array.isArray(accounts) && accounts.length > 0
-        ? accounts[0]
-        : null
-    })
-
-    // export web3 as a global, checking for usage
-    let reloadInProgress = false
-    let lastTimeUsed
-    let previousChainId
-
-    const web3Proxy = new Proxy(web3, {
-      get: (_web3, key) => {
-        // get the time of use
-        lastTimeUsed = Date.now()
-        // return value normally
-        return _web3[key]
-      },
-      set: (_web3, key, value) => {
-        // set value normally
-        _web3[key] = value
-      },
-    })
-
-    Object.defineProperty(window, 'web3', {
-      enumerable: false,
-      writable: true,
-      configurable: true,
-      value: web3Proxy,
-    })
-
-    window.ethereum.on('chainChanged', (currentChainId) => {
-      // if the auto refresh on network change is false do not
-      // do anything
-      if (!window.ethereum.autoRefreshOnNetworkChange) {
-        return
-      }
-
-      // if reload in progress, no need to check reload logic
-      if (reloadInProgress) {
-        return
-      }
-
-      // set the initial chain
-      if (!previousChainId) {
-        previousChainId = currentChainId
-        return
-      }
-
-      // skip reload logic if web3 not used
-      if (!lastTimeUsed) {
-        return
-      }
-
-      // if chain did not change, exit
-      if (currentChainId === previousChainId) {
-        return
-      }
-
-      // initiate page reload
-      reloadInProgress = true
-      const timeSinceUse = Date.now() - lastTimeUsed
-      // if web3 was recently used then delay the reloading of the page
-      if (timeSinceUse > 500) {
-        window.location.reload()
-      } else {
-        setTimeout(window.location.reload, 500)
-      }
-    })
-  }
+    // initiate page reload
+    reloadInProgress = true
+    const timeSinceUse = Date.now() - lastTimeUsed
+    // if web3 was recently used then delay the reloading of the page
+    if (timeSinceUse > 500) {
+      window.location.reload()
+    } else {
+      setTimeout(window.location.reload, 500)
+    }
+  })
 }
