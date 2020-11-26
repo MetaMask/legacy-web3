4b97c387f6dc009097857d2e0536dad158f6408b
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2020-11-25 21:44:32 -0800

    Fix Node.js require (#22)
    * Enable requiring package via Node.js
    
    * Use umd format
    
    * lint

--- rollup.config.js
+++ rollup.config.js
@@ -4,15 +4,23 @@ import { terser } from 'rollup-plugin-terser'
 
 module.exports = {
   input: 'src/web3Wrapper.js',
-  output: {
-    file: 'dist/metamask.web3.min.js',
-    format: 'iife',
-  },
+  output: [
+    {
+      file: 'dist/metamask.web3.min.js',
+      format: 'iife',
+      plugins: [
+        terser(),
+      ],
+    },
+    {
+      file: 'dist/index.js',
+      format: 'umd',
+    },
+  ],
   plugins: [
     resolve(),
     commonjs({
       sourceMap: false,
     }),
-    terser(),
   ],
 }
